a8e314bd14a641752753b13f0bcc7a3b08f71bf0
nabil <yampolos@gmail.com>
2016-06-29 22:28:25 +0200

    test du script

--- scriptscroll.js
+++ scriptscroll.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){       
    var scroll_start = 0;
-   var startchange = $('#change');
+   var startchange = $('#myCarousel');
    var offset = startchange.offset();
     if (startchange.length){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-inverse").css('background-color', '#f0f0f0');
+          $('.navbar-header').css('visibility','visible');
        } else {
-          $('.navbar-inverse').css('background-color', 'transparent');
+          $('.navbar-header').css('visibility','hidden');
        }
    });
     }
